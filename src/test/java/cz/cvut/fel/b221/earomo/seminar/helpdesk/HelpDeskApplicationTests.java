a648a845ca0d348898f8f98b3b5996f9fb8f2c1a
Adam Roth <rothadam@Adams-MacBook-Pro.local>
2022-11-04 12:22:25 +0100

    Spring Boot Init

---
+++ src/test/java/cz/cvut/fel/b221/earomo/seminar/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fel.b221.earomo.seminar.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
